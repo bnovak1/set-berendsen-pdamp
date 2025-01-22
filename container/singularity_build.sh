ea9aa15ba0d7ce5a113d88a8fd3933e223de4e5b
Brian Novak <bnovak1@users.noreply.github.com>
2025-01-22 13:04:50 -0700

    Build Singularity image using a definition file (.def) instead of 1st building a Docker image and converting it

--- container/singularity_build.sh
+++ container/singularity_build.sh
@@ -7,8 +7,11 @@ LAMMPS_VERSION=${2:-2Aug2023}
 
 # Output directory
 outdir=/home/bnovak1/singularity/set_berendsen_pdamp
+mkdir -p ${outdir}
 
-# Build singularity image from docker image
-singularity build ${outdir}/python_${PYTHON_VERSION}_lammps_${LAMMPS_VERSION,,}.sif \
-                  docker-daemon:python_${PYTHON_VERSION}_lammps_${LAMMPS_VERSION,,}:latest
-ln -s -n ${outdir}/python_${PYTHON_VERSION}_lammps_${LAMMPS_VERSION,,}.sif
+# Build singularity image from definition file
+sudo singularity build ${outdir}/python_${PYTHON_VERSION}_lammps_${LAMMPS_VERSION,,}.sif \
+                  container/python_${PYTHON_VERSION}_lammps_${LAMMPS_VERSION,,}.def
+
+# Create symlink in container directory
+ln -s -f ${outdir}/python_${PYTHON_VERSION}_lammps_${LAMMPS_VERSION,,}.sif container/python_${PYTHON_VERSION}_lammps_${LAMMPS_VERSION,,}.sif
